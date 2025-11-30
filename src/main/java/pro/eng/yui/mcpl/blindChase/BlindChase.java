d1035e826fa9d5ddea0db282516f8bbab86ee281
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-11-30 19:31:24 +0900

    initial commit

---
+++ src/main/java/pro/eng/yui/mcpl/blindChase/BlindChase.java
@@ -0,0 +1,17 @@
+package pro.eng.yui.mcpl.blindChase;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BlindChase extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
