bd12abe26c4ba023a7a7cc4a8cfc04ee0b6eaf16
yui-Kitamura <yui@yui-kitamura.eng.pro>
2025-12-05 16:43:46 +0900

    fix Color→ChatColor

--- src/main/java/pro/eng/yui/mcpl/blindChase/lib/resourcepack/ResourcePackStatusListener.java
+++ src/main/java/pro/eng/yui/mcpl/blindChase/lib/resourcepack/ResourcePackStatusListener.java
@@ -1,6 +1,6 @@
 package pro.eng.yui.mcpl.blindChase.lib.resourcepack;
 
-import org.bukkit.Color;
+import org.bukkit.ChatColor;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerResourcePackStatusEvent;
@@ -11,11 +11,11 @@ public class ResourcePackStatusListener implements Listener {
     public void onPackStatus(PlayerResourcePackStatusEvent e) {
         switch (e.getStatus()) {
             case DECLINED:
-                e.getPlayer().sendMessage(Color.RED + "（警告）リソースパックが拒否されました");
+                e.getPlayer().sendMessage(ChatColor.RED + "（警告）リソースパックが拒否されました" + ChatColor.RESET);
                 break;
             case FAILED_DOWNLOAD:
             case FAILED_RELOAD:
-                e.getPlayer().sendMessage(Color.RED + "（警告）リソースパックのダウンロード/適用に失敗しました");
+                e.getPlayer().sendMessage(ChatColor.RED + "（警告）リソースパックのダウンロード/適用に失敗しました" + ChatColor.RESET);
                 break;
             case ACCEPTED:
             case SUCCESSFULLY_LOADED:
